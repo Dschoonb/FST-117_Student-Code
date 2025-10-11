959d1905cc56ff936efc5f90b45e174d7b2738e3
Dschoonb <dschoonbrood@ucdavis.edu>
2025-10-11 12:30:07 -0700

    Modified Week 5 Discusssion

--- Week 5/Discussion 5.R
+++ Week 5/Discussion 5.R
@@ -1,46 +1,38 @@
 ## Discussion 5
-
-# Cleaning Data -----------------------------------------------------------
 #This weeks dataset contains data from a sensory descriptive analysis panel which assessed liquids containing different thickening agents.
-#Load up the dataset.
+
+
+# Load Data -----------------------------------------------------------
 library(readxl)
 liquids <- read_excel("Week 5/liquids.xlsx")
-View(liquids)
 
-#The dataset contains some junk in the last few rows.
-#We need to get rid of this junk and give the sample column a better name.
+
+# Clean Data --------------------------------------------------------------
 library(tidyverse)
 liquids <- liquids[,c(1:7)]
 liquids <- rename_at(liquids,3,~"Sample")
 
 
 # Making Factors, Factors -------------------------------------------------
-#Since we are looking at categorical explanatory variables we need to have that reflected in the code
-#First check the structure of the data
-str(liquids)
-#then use the as.factor function to make the column a factor
-liquids$Session <- as.factor(liquids$Session)
-liquids$Panelist <- as.factor(liquids$Panelist)
-liquids$Sample <- as.factor(liquids$Sample)
-liquids$gender <- as.factor(liquids$gender)
-str(liquids)
-#Check the structure again to make sure it worked
-#use can use as.numeric to revert any changes to a column
+#Since we are looking at categorical predictors we need to have that reflected in the code
+str(liquids) #Check the structure of the data
+liquids[,c(1:3,7)] <- lapply(liquids[,c(1:3,7)], as.factor)
+str(liquids) #Check the structure again
 
 
-#Visualization before the test -----------------------------------------------------------
-#Lets take a peak at the spread of out data first
+#Visualize Data----------------------------------------------------------
 library(ggplot2)
-anova_plot <- ggplot(data=liquids) + geom_boxplot(aes(x=Sample, y=thickness))
-lm_plot <- ggplot(data=liquids) + geom_point(aes(x=Sample, y=thickness))
+#plot this in histrogram form
+ggplot(data=liquids) + geom_histogram(aes(x=thickness), position = "dodge", bins = 15)
+#add sample explanatory variable
+ggplot(data=liquids) + geom_histogram(aes(x=thickness, fill=Sample), bins = 15)
+#scatter plot
+ggplot(data=liquids) + geom_point(aes(x=Sample, y=thickness, color=Sample))
 
 
-
-# Performing a 1-Way ANOVA ------------------------------------------------
-# 1-way ANOVA
-liquids.anova <- aov(thickness~Sample, data = liquids)
-summary(liquids.anova)
-
+# ANOVA ------------------------------------------------
+liquids.anova <- lm(thickness~Sample, data = liquids)
+anova(liquids.anova)
 
 
 # Multiple Mean Comparison Tests -----------------------------------------
@@ -67,8 +59,10 @@ LSDresult <- LSD.test(liquids.anova, trt = "Sample", console = TRUE, alpha = 0.0
 #Visualize the data with bar charts
 ggplot(means_methods, aes(x=Sample, y=thickness)) + geom_bar(stat = "identity") + 
   geom_text(label=c(LSDresult$groups$groups), mapping = aes(x=Sample, y=thickness+0.5))
-
-
+#violin plot
+ggplot(liquids, aes(x=Sample, y=thickness)) + geom_violin() + 
+  stat_summary(fun=mean, geom="point", size=2, color="red") +
+  geom_text(data=means_methods, label=c(LSDresult$groups$groups), mapping = aes(x=Sample, y=thickness+0.5))
 
 
 
