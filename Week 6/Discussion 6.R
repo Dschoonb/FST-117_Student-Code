676e45ffb66e819a4442864dd5505bde373faef6
Dschoonb <dschoonbrood@ucdavis.edu>
2025-10-13 11:28:07 -0700

    Edited Discussion 6

--- Week 6/Discussion 6.R
+++ Week 6/Discussion 6.R
@@ -2,16 +2,11 @@
 library(readxl)
 liquids <- read_excel("Week 6/liquids.xlsx")
 
-# Clean Data --------------------------------------------------------------
-library(tidyverse)
-liquids <- liquids[,c(1:7)]
-liquids <- rename_at(liquids,3,~"Sample")
-
-
 # Making Factors, Factors -------------------------------------------------
 #Since we are looking at categorical predictors we need to have that reflected in the code
+library(tidyverse)
 str(liquids) #Check the structure of the data
-liquids[,c(1:3,7)] <- lapply(liquids[,c(1:3,7)], as.factor)
+liquids[,c(1:4)] <- lapply(liquids[,c(1:4)], as.factor)
 str(liquids) #Check the structure again
 
 
@@ -33,7 +28,11 @@ anova(model2int)
 
 # Visualizing interactions ------------------------------------------------
 library(ggplot2)
-ggplot(data=liquids, mapping = aes(x=Sample, y=stickiness, group=Panelist, color=Panelist)) + geom_point()+stat_summary(aes(y = stickiness, group=Panelist, color=Panelist), fun=mean, geom="line")
+a <- 
+  liquids %>% 
+  ggplot(aes(x=Sample, y=stickiness, group=Panelist, color=Panelist)) + 
+  geom_point() +
+  stat_summary(aes(y = stickiness, group=Panelist, color=Panelist), fun=mean, geom="line")
 
 #Separating Data by panelist using facet_wrap function
-ggplot(data=liquids, mapping = aes(x=Sample, y=stickiness, group=Panelist, color=Panelist)) + geom_point()+stat_summary(aes(y = stickiness, group=Panelist, color=Panelist), fun=mean, geom="line") + facet_wrap(vars(Panelist))
+a1 <- a + facet_wrap(~Panelist)
