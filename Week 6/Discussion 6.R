8ae3d0321dbdb1b0572feba366f7b698437fc0ae
Dschoonb <dschoonbrood@ucdavis.edu>
2025-10-12 22:17:22 -0700

    Worked on Discussion 5 and 6

--- Week 6/Discussion 6.R
+++ Week 6/Discussion 6.R
@@ -2,61 +2,37 @@
 library(readxl)
 liquids <- read_excel("Week 6/liquids.xlsx")
 
-#Setting our factors
-str(liquids)
-liquids$Session <- as.factor(liquids$Session)
-liquids$Panelist <- as.factor(liquids$Panelist)
-liquids$Sample <- as.factor(liquids$Sample)
-liquids$gender <- as.factor(liquids$gender)
-str(liquids)
-
-#Changing a factor back to a numeric
-liquids$stickiness <- as.factor(liquids$stickiness)
-str(liquids)
-liquids$stickiness <- as.numeric(liquids$stickiness)
-str(liquids)
+# Clean Data --------------------------------------------------------------
+library(tidyverse)
+liquids <- liquids[,c(1:7)]
+liquids <- rename_at(liquids,3,~"Sample")
 
 
+# Making Factors, Factors -------------------------------------------------
+#Since we are looking at categorical predictors we need to have that reflected in the code
+str(liquids) #Check the structure of the data
+liquids[,c(1:3,7)] <- lapply(liquids[,c(1:3,7)], as.factor)
+str(liquids) #Check the structure again
 
 
+# ANOVA -------------------------------------------------------------------
 # Refresher - 1-way ANOVA
-stickiness_1way <- aov(stickiness~Sample, liquids)
-summary(stickiness_1way)
-
-
-
+model1 <- lm(thickness~Sample, data = liquids)
+anova(model1)
 
 # 2-way ANOVA
-stickiness_2way <- aov(stickiness~Sample+Panelist, liquids)
-summary(stickiness_2way)
+model2 <- lm(thickness~Sample+Panelist, liquids)
+anova(model2)
+
+# 2-way ANOVA with interaction
+model2int <- lm(thickness~Sample*Panelist, liquids)
+anova(model2int)
 
 
 
 
-# 2-way ANOVA with 1-way interaction
-stickiness_2way1way1 <- aov(stickiness~Sample*Panelist, liquids)
-summary(stickiness_2way1way1)
-
-stickiness_2way1way1 <- aov(stickiness~(Sample+Panelist)^2, liquids)
-summary(stickiness_2way1way1)
-
-
-#above shows two ways of running the same model
-
-stickiness_2way1way2 <- aov(stickiness~Sample*Session,liquids)
-summary(stickiness_2way1way2)
-
-stickiness_2way1way3 <- aov(stickiness~Sample*gender,liquids)
-summary(stickiness_2way1way3)
-
-
-
-
-
-#Plotting our data to visualize interaction effects
+# Visualizing interactions ------------------------------------------------
 library(ggplot2)
-liquids$Sample <- as.numeric(liquids$Sample)
-
 ggplot(data=liquids, mapping = aes(x=Sample, y=stickiness, group=Panelist, color=Panelist)) + geom_point()+stat_summary(aes(y = stickiness, group=Panelist, color=Panelist), fun=mean, geom="line")
 
 #Separating Data by panelist using facet_wrap function
